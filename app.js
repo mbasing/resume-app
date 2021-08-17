460756bd6bdce0b6e4fdf403f57d02b94002ae7d
Moses Basing <mosesbasing@gmail.com>
2021-08-17 14:13:38 +0000

    Add a simple program that prints out a user's name

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+// A simple JavaScript program that prints a user's name
 
+const getUserName = (firstName, lastName) => {
+    console.log(`${firstName} ${lastName}`);
+  };
+  
+  getUserName("Moses", "Basing");
